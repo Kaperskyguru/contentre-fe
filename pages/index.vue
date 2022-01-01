edb380e33258164aa7efcd93d31fff1c71226270
Solomon Eseme <kaperskyguru@gmail.com>
2022-01-01 13:53:10 +0100

    chore: move files and update path

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,53 @@
 <template>
   <div>
-    <AuthPageTitle />
-    <AuthInputField placeholder="www.contentr.me/ username" label="Choose your portfolio address" />
-    <AuthInputField placeholder="Enter your full name" label="Full name" />
-    <AuthInputField placeholder="Enter your email address" label="Email address" />
-    <AuthInputField placeholder="Create password" label="Create a strong password" type="password" />
-    <AuthInputField placeholder="Re-enter password" label="Re-enter your password" type="password" />
-    <AuthCorrectPasswordIdentifier />
-    <AuthCorrectPasswordIdentifier correct="true" text="Password should contain a special character" />
-    <AuthCorrectPasswordIdentifier text="Password should have an uppercase letter" />
-    <AuthSubmitButton>Create Account</AuthSubmitButton>
-    <p class="gilroy-bold text-center">Already have an account? <NuxtLink to="sign-in">Sign In</NuxtLink></p>
+    <PageTitle />
+    <InputField
+      placeholder="www.contentr.me/ username"
+      label="Choose your portfolio address"
+    />
+    <InputField placeholder="Enter your full name" label="Full name" />
+    <InputField placeholder="Enter your email address" label="Email address" />
+    <InputField
+      placeholder="Create password"
+      label="Create a strong password"
+      type="password"
+    />
+    <InputField
+      placeholder="Re-enter password"
+      label="Re-enter your password"
+      type="password"
+    />
+    <CorrectPasswordIdentifier />
+    <CorrectPasswordIdentifier
+      correct
+      text="Password should contain a special character"
+    />
+    <CorrectPasswordIdentifier
+      text="Password should have an uppercase letter"
+    />
+    <SubmitButton>Create Account</SubmitButton>
+    <p class="gilroy-bold text-center">
+      Already have an account? <NuxtLink to="sign-in">Sign In</NuxtLink>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  layout: "AuthLayout"
+  layout: 'AuthLayout',
 }
 </script>
 
 <style scoped>
 p {
-  color: #102A27;
+  color: #102a27;
   font-size: 16px;
   line-height: 24px;
   letter-spacing: 0.02em;
 }
 p a {
-  color: #4FD1C5;
+  color: #4fd1c5;
   text-decoration: none;
 }
 </style>
