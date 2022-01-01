d260593b7bf67d0ab7198998d287e89e959cbb3a
Solomon Eseme <kaperskyguru@gmail.com>
2022-01-01 12:33:56 +0100

    fix: fix App name

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,9 +15,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    "~/plugins/bootstrap-vue.js"
-  ],
+  plugins: ['~/plugins/bootstrap-vue.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -55,6 +53,6 @@ export default {
   build: {},
 
   env: {
-    APP_NAME: "Contentr"
-  }
+    APP_NAME: 'Contentre',
+  },
 }
