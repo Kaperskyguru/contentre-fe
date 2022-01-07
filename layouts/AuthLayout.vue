3f4ee01083569236ec71ec8a49cb51e4bdc3aa8a
Solomon Eseme <kaperskyguru@gmail.com>
2022-01-07 15:46:46 +0100

    chore: config and more svg

--- layouts/AuthLayout.vue
+++ layouts/AuthLayout.vue
@@ -1,24 +1,51 @@
 <template>
-<div class="relative lg:flex">
-  <NavBar />
-  <div class="bg-auth-background lg:min-h-screen bg-center bg-no-repeat bg-cover w-full py-4 px-4 min-h-80vh flex justify-center items-center lg:flex-1 lg:items-start lg:pt-20 lg:pl-10vw">
-    <div class="lg:pt-20 lg:mt-20">
-      <h1 class="text-white text-xl mb-6 font-gilroy font-bold tracking-wide sm:text-3xl">Paste your Author Link and get all your contents in one place.</h1>
-      <p class="font-gilroy text-white font-medium tracking-wide">Save all your writings and articles in one platform and view the statistics.</p>
+  <div class="relative lg:flex">
+    <NavBar />
+    <div
+      class="
+        bg-auth-background
+        lg:min-h-screen
+        bg-center bg-no-repeat bg-cover
+        w-full
+        py-4
+        px-4
+        min-h-80vh
+        flex
+        justify-center
+        items-center
+        lg:flex-1 lg:items-start lg:pt-20 lg:pl-10vw
+      "
+    >
+      <div class="lg:pt-20 lg:mt-20">
+        <h1
+          class="
+            text-white text-xl
+            mb-6
+            font-gilroy font-bold
+            tracking-wide
+            sm:text-3xl
+          "
+        >
+          Paste your Author Link and get all your contents in one place.
+        </h1>
+        <p class="font-gilroy text-white font-medium tracking-wide">
+          Save all your writings and articles in one platform and view the
+          statistics.
+        </p>
+      </div>
     </div>
+    <div class="lg:flex-1 lg:pt-20 lg:pl-7 lg:min-h-screen lg:pr-10vw">
+      <Nuxt />
+    </div>
+    <Toast />
   </div>
-  <div class="lg:flex-1 lg:pt-20 lg:pl-7 lg:min-h-screen lg:pr-10vw">
-    <Nuxt />
-  </div>
-</div>
 </template>
 
 <script>
 export default {
-  name: "AuthLayout"
+  name: 'AuthLayout'
 }
 </script>
 
 <style scoped>
-
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,41 @@
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
   ],
   theme: {
     extend: {
       colors: {
-        'primary-teal': "#4FD1C5",
-        'header-text-color': "#286963",
+        'primary-teal': '#4FD1C5',
+        'header-text-color': '#286963',
         'body-text-color': '#102A27',
-        'placeholder': '#BDBDBD',
+        placeholder: '#BDBDBD',
         'border-inner': '#99999999',
         'default-grey': '#c4c4c4',
         'grey-shade': '#88888888',
+        current: 'currentColor'
       },
       fontFamily: {
         gilroy: ['Gilroy', 'sans-serif'],
-        helvetica: ['Helvetica', 'sans-serif'],
+        helvetica: ['Helvetica', 'sans-serif']
       },
       backgroundImage: {
-        'auth-background': "url(~assets/img/auth-nav/backdrop.svg)"
+        'auth-background': 'url(~assets/img/auth-nav/backdrop.svg)'
       },
       minHeight: {
         'half-screen': '50vh',
-        '80vh': '80vh',
+        '80vh': '80vh'
       },
       width: {
         '80vw': '80vw'
       },
       padding: {
-        '10vw': '10vw',
+        '10vw': '10vw'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
