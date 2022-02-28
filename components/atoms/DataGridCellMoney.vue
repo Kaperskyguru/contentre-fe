2c255bd1172fa91ace3f452f7fa7831abb01424e
Solomon Eseme <kaperskyguru@gmail.com>
2022-02-28 11:43:34 +0100

    feat: add new components

---
+++ components/atoms/DataGridCellMoney.vue
@@ -0,0 +1,30 @@
+<template>
+  <AccountingLabel
+    :value="value"
+    :currency="currency"
+    :currency-before="options.currencyBefore"
+  />
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import BaseCell from './DataGridCellBase'
+import AccountingLabel from '~/components/molecules/AccountingLabel.vue'
+
+export default defineComponent({
+  name: 'DataGridCellMoney',
+
+  components: {
+    AccountingLabel
+  },
+
+  extends: BaseCell,
+
+  computed: {
+    currency() {
+      const currencyKey = this.options.currencyKey || 'currency'
+      return this.item[currencyKey] ?? this.options[currencyKey]
+    }
+  }
+})
+</script>
