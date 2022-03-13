62fb3efbaecfce9a39a92b20c4f888361ca6f1c0
Solomon Eseme <kaperskyguru@gmail.com>
2022-03-13 14:22:17 +0100

    chore: remove customizer

--- layouts/SettingsLayout.vue
+++ layouts/SettingsLayout.vue
@@ -1,16 +1,5 @@
 <template>
   <main>
-    <!-- <nav class="fixed z-30 w-full bg-white border-b border-gray-200 navside-bs">
-      <Nav @logout="onLogout" />
-    </nav>
-    <div class="flex pt-16">
-      <Aside />
-    </div>
-
-    <section class="px-3 h-full md:px-12">
-      <div class="flex justify-between items-center py-4">
-        <PageTitle>Settings</PageTitle>
-      </div> -->
     <section class="px-3 h-full md:px-12">
       <div class="container mx-5 md:flex md:-mx-2">
         <div class="w-full bg-white rounded-xl shadow md:mx-2 md:w-5/12">
@@ -33,8 +22,6 @@
 import Dashboard from './Dashboard.vue'
 export default {
   name: 'DashboardNav',
-
-  // layout: 'Dashboard',
 
   extends: Dashboard,
 
