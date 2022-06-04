ddc0ac952448d78b3204fac3ff6ecd1775ea7e47
Solomon Eseme <kaperskyguru@gmail.com>
2022-06-04 18:03:03 +0100

    chore: remove console.log

--- middleware/checkSubscription.js
+++ middleware/checkSubscription.js
@@ -1,10 +1,7 @@
 export default ({ route, redirect, $getCurrentUser, $toast }) => {
   try {
     // Force load any logged in user first.
-
     // Allow access route only when user is logged in.
-
-    console.log('HETE')
   } catch (error) {
     $toast.message = error.message
   }
