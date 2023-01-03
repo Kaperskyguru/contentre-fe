b497aa4c594f1d80e2ef665134b52f5f6f69d4eb
Solomon Eseme <kaperskyguru@gmail.com>
2023-01-03 15:41:47 +0100

    fix: remove scroll and fix notebook page

--- components/molecules/PageContent.vue
+++ components/molecules/PageContent.vue
@@ -6,12 +6,7 @@
       pb-2
       space-y-6
       h-full
-      md:overflow-hidden
-      md:overflow-y-hidden
-      md:flex-row
-      md:px-6
-      md:space-y-0
-      md:space-x-6
+      md:flex-row md:px-6 md:space-y-0 md:space-x-6
       no-scrollbar
     "
     :class="{
@@ -29,23 +24,9 @@
       <slot name="sidebar" />
     </aside>
 
-    <div class="flex-1 h-full md:overflow-y-hidden no-scrollbar">
-      <div
-        class="
-          flex
-          overflow-hidden
-          flex-col
-          space-y-6
-          w-full
-          drop-shadow
-          md:overflow-y-auto md:h-full
-          no-scrollbar
-        "
-        :class="{ 'items-center justify-center': waiting }"
-      >
-        <Loading v-if="waiting" class="opacity-20" />
-        <slot v-else />
-      </div>
+    <div class="flex flex-col flex-1 space-y-6 drop-shadow">
+      <Loading v-if="waiting" class="opacity-20" />
+      <slot v-else />
     </div>
 
     <hr class="block grow-0 shrink-0 h-10 md:hidden" />
